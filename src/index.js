e1709af7384d4cbd8e8985b19be42ddb9962ab35
Md Nazmul Haque <48866588+bpinazmul18@users.noreply.github.com>
2021-03-09 14:57:43 +0600

    add store, store.dispatch, getState, subscribe and unsubscribe

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import store from "./store";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+const unsubscribe = store.subscribe(() => {
+  console.log("Store changed", store.getState());
+});
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+store.dispatch({
+  type: "bugAdded",
+  payload: {
+    description: "Bug1",
+  },
+});
+
+unsubscribe();
+
+store.dispatch({
+  type: "bugRemoved",
+  payload: {
+    id: 1,
+  },
+});
+
+console.log(store.getState());
