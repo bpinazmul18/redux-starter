7103f3e6898648306870cb5de0bb916aa805a5d5
Md Nazmul Haque <48866588+bpinazmul18@users.noreply.github.com>
2021-03-09 16:43:01 +0600

    add private properties

--- src/index.js
+++ src/index.js
@@ -1,16 +1,3 @@
-import store from "./store";
-import { bugAdded, bugRemoved, bugResolved } from "./actions";
-
-// const unsubscribe = store.subscribe(() => {
-//   console.log("Store changed", store.getState());
-// });
-
-store.dispatch(bugAdded("Bug1"));
-
-// unsubscribe();
-
-// store.dispatch(bugRemoved(1));
-
-store.dispatch(bugResolved(1));
-
-console.log(store.getState());
+import store from "./customState";
+store.state = 1;
+console.log(store);
