812dfbed8ea1f9adfac623da6a15ed1f4678328a
Md Nazmul Haque <48866588+bpinazmul18@users.noreply.github.com>
2021-03-10 14:21:34 +0600

    file structure, ducks pattern, redux-toolkit and also createStore

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
-import store from "./store";
-import * as actions from "./actions";
+import configureStore from "./store/configureStore";
+import * as actions from "./store/bugs";
+
+const store = configureStore();
 
 store.subscribe(() => {
-  console.log("Store changed!", store.getState());
+  console.log("Store change", store.getState());
 });
-store.dispatch(actions.bugAdded("Bug1"));
+
+store.dispatch(actions.bugAdded("Bug 1"));
+store.dispatch(actions.bugAdded("Bug 2"));
+store.dispatch(actions.bugAdded("Bug 3"));
+store.dispatch(actions.bugResolved(1));
 
 console.log(store.getState());
