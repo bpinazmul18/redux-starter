4dcaf929bad7f9ba27917bc953617e934c5405a4
Md Nazmul Haque <48866588+bpinazmul18@users.noreply.github.com>
2021-03-13 19:05:03 +0600

    add actions

--- src/index.js
+++ src/index.js
@@ -59,14 +59,14 @@
 // });
 
 import configureStore from "./store/configureStore";
+import * as actions from "./store/api";
 
 const store = configureStore();
 
-store.dispatch({
-  type: "apiCallBegan",
-  payload: {
+store.dispatch(
+  actions.apiCallBegan({
     url: "/bugs",
     onSuccess: "bugsReceived",
-    onError: "apiRequestFailed",
-  },
-});
+    onError: actions.apiCallFailed.type,
+  })
+);
---
+++ src/store/api.js
@@ -0,0 +1,5 @@
+const { createAction } = require("@reduxjs/toolkit");
+
+export const apiCallBegan = createAction("api/callBegan");
+export const apiCallSuccess = createAction("api/callSuccess");
+export const apiCallFailed = createAction("api/callFailed");
