b8c2ffaaf16e74aec7c8d4710704f62b98673bb2
Md Nazmul Haque <48866588+bpinazmul18@users.noreply.github.com>
2021-03-14 15:21:01 +0600

    add first test using jest

---
+++ src/math.spec.js
@@ -0,0 +1,17 @@
+import { isEven } from "./math";
+
+describe("isEven", () => {
+  it("Should return true if given an even number", () => {
+    // Function under test (SUT)
+
+    const result = isEven(2);
+    expect(result).toEqual(true);
+  });
+
+  it("Should return false if given an odd number", () => {
+    // Function under test (SUT)
+
+    const result = isEven(2);
+    expect(result).toEqual(false);
+  });
+});
