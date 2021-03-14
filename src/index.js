3fd296988c9eb72df8a6b3408c4efb2ba8524f9d
Md Nazmul Haque <48866588+bpinazmul18@users.noreply.github.com>
2021-03-14 10:52:37 +0600

    getting data from the server

--- src/index.js
+++ src/index.js
@@ -57,15 +57,8 @@
 //   type: "error",
 //   payload: { message: "An error occurred" },
 // });
-
 import configureStore from "./store/configureStore";
-import * as actions from "./store/api";
-
+import { loadBugs } from "./store/bugs";
 const store = configureStore();
 
-store.dispatch(
-  actions.apiCallBegan({
-    url: "/bugs",
-    onSuccess: "bugsReceived",
-  })
-);
+store.dispatch(loadBugs());
--- src/store/bugs.js
+++ src/store/bugs.js
@@ -1,5 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { createSelector } from "reselect";
+import { apiCallBegan } from "./api";
 
 let lastId = 0;
 
@@ -11,6 +12,9 @@ const slice = createSlice({
     lastFetch: null,
   },
   reducers: {
+    bugsReceived: (bugs, action) => {
+      bugs.list = action.payload;
+    },
     bugAssignedToUser: (bugs, action) => {
       const { bugId, userId } = action.payload;
       const index = bugs.list.findIndex((bug) => bug.id === bugId);
@@ -30,8 +34,22 @@ const slice = createSlice({
   },
 });
 
-export const { bugAdded, bugResolved, bugAssignedToUser } = slice.actions;
+export const {
+  bugAdded,
+  bugResolved,
+  bugAssignedToUser,
+  bugsReceived,
+} = slice.actions;
 export default slice.reducer;
+
+//Action Creators
+const url = "/bugs";
+
+export const loadBugs = () =>
+  apiCallBegan({
+    url,
+    onSuccess: bugsReceived.type,
+  });
 
 //Selector
 //Memorization
